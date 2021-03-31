2a48bfea2b2f97c44ce2e6f4eb8cea0e22658beb
bjexe <bensimms@knights.ucf.edu>
2021-03-31 03:27:17 -0400

    Adding initial home screen

--- mobile/components/Home.js
+++ mobile/components/Home.js
@@ -1,8 +1,9 @@
 import React, { Component, useState } from 'react';
-import { Text, View, Image, Button } from 'react-native';
+import { Text, View, Image, Button, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native';
 import { Navigation_Container, Welcome_Message } from './style';
-
+import { LinearGradient } from 'expo-linear-gradient';
 import { useNavigation } from '@react-navigation/native';
+import { Icon } from 'react-native-elements';
 
 
 
@@ -14,61 +15,29 @@ const LogOutButton = ({props, navigation}) => {
     
     navigation = useNavigation();
 
+    //button wrapped in view because buttons cannot be resized but views can
+    //alignSelf: "flex-end" puts the button to the end of the screen horizontally
+    //margin: 10 was a nice number to keep the button from bordering the screen too closely
     return (
-        
-        <Button
-            onPress = {() => {
-                //setIsLoggedIn(false);
-                navigation.navigate('Login');
-            }}
-            title = "Logout"
-            color = "#FF0000"
-            alignSelf = "flex-end"
-        >
-            {/* onclick = props.setIsLoggedIn(false) */}
-            {/* style =  */}
-        </Button>
+        <View style = {{
+            width: 75,
+            alignSelf: "flex-end",
+            margin: 10
+            }}> 
+            <Button
+                onPress = {() => {
+                    //setIsLoggedIn(false);
+                    navigation.navigate('Login');
+                }}
+                title = "Logout"
+                color = "#F08080"
+                alignSelf = "flex-end"
+            >
+                {/* onclick = props.setIsLoggedIn(false) */}
+                {/* style =  */}
+            </Button>
+        </View>
     );
-}
-
-//list of user-made lists
-const TodoListList = () => {
-
-    //return (
-
-    //    );
-
-}
-
-//TodoListList item (individual list)
-const ListItem = () => {
-    
-    //return (
-
-    //    );
-
-}
-
-//buttons that allow to filter lists (important, finished, etc)
-const FilterTasks = () => {
-
-    //return (
-
-    //    );
-    
-}
-
-//dividing line between top and bottom views
-const DividingLine = () => {
-
-    //<View style = {StyleSheet.separator} />
-    /* put below into a stylesheet:
-    separator: {
-        marginVertical: 8,
-        borderBottomColor: '#737373',
-        borderBottomWidth: StyleSheet.hairlineWidth,
-    }
-    */
 }
 
 //Button with "+" icon to add a task-list
@@ -77,22 +46,134 @@ const AddButton = () => {
     //color likely to change
     //bring up modal/new page to add a task-list
     return (
-        <Button
-        title = "+"
-        color = "F6C2B7"
-        >
-            
-        </Button>
+        <View style = {{
+            width: 45,
+            alignSelf: "flex-end",
+            marginRight: 35
+        }}>
+            <Icon raised name = "add-outline" type = 'ionicon'>
+
+            </Icon>
+        </View> 
     );
 
 }
 
-//scrollable view that shows all task-lists (loads TaskListList items)
-const TaskList = () => {
+//buttons that allow to filter lists (important, finished, etc)
+const FilterTasks = () => {
 
-    //return (
+    return (
+        <View style = {{
+            margin: 35
+        }}>
+            <View style = {{flexDirection: 'row', alignItems: 'center'}}>
+                <Icon raised name = "folder-outline" type = 'ionicon' />
+                <Text style={{ fontWeight: 'bold' }}>All Tasks</Text>
+            </View>
+            <View style = {{flexDirection: 'row', alignItems: 'center'}}>
+                <Icon raised name = "star" type = 'ionicon' />
+                <Text style={{ fontWeight: 'bold' }}>Important</Text>
+            </View>
+            <View style = {{flexDirection: 'row', alignItems: 'center'}}>
+                <Icon raised name = "checkmark" type = 'ionicon' />
+                <Text style={{ fontWeight: 'bold' }}>Completed</Text>
+            </View>
+        </View>
+    );
+    
+}
 
-    //);
+//scrollable view of user-made lists
+//currently has arbitrary list items, supposed to grab from api
+const ListGroup = () => {
+
+    return (
+        <SafeAreaView>
+            <ScrollView>
+                <TodoListItem/>
+                <TodoListItem/>
+                <TodoListItem/>
+                <TodoListItem/>
+                <TodoListItem/>
+                <TodoListItem/>
+                <TodoListItem/>
+                <TodoListItem/>
+            </ScrollView>
+        </SafeAreaView>
+    );
+
+}
+
+//TodoListList item (individual list)
+const TodoListItem = () => {
+    
+    return (
+        <View style = {{
+            flexDirection: "row"
+        }}>
+            <Icon raised name = "create-outline" type = 'ionicon' />
+            <Icon raised name = "arrow-forward-outline" type = 'ionicon' />
+            <Text style={{
+                fontWeight: 'bold',
+                alignSelf: "center",
+                marginLeft: 20 }}>I'm an example list!</Text>
+        </View>
+    );
+
+}
+
+//contains logout button, welcome message, and avatar (todo)
+//maybe mess with welcome message placement
+const Header = () => {
+
+    return (
+        <View style = {{
+            flexDirection: 'row',
+            justifyContent: "space-between"
+        }}>
+            <Text style = {{alignSelf: "center", marginLeft: 75}}>
+            Welcome, [user]
+            </Text>
+            <LogOutButton/>
+        </View>
+    );
+
+}
+
+//dividing line view
+const DividingLine = () => {
+
+    return (
+        <View style = {{
+            marginVertical: 2,
+            borderBottomColor: '#030608',
+            borderBottomWidth: 2
+        }}/>
+    );
+    
+}
+
+const TopView = () => {
+
+    return (
+        <View style = {{
+            //flex:1 ---IMPORTANT--- (breaks the whole screen lmao but should make the scrollview work. parent view for scrollview needs flex:1?)
+        }}>
+            <Header/>
+            <FilterTasks/>
+        </View>
+    );
+
+}
+
+const BottomView = () => {
+
+    return (
+        <View>
+            <AddButton/>
+            <ListGroup/>
+        </View>
+    );
 
 }
 
@@ -103,10 +184,15 @@ const HomePage = () => {
 
 
     return (
+        
         <View>
-            <LogOutButton/>
-            {/*add other two views with necessary components*/}
+            <LinearGradient colors={['#96CAF7', '#FFF']}>
+                <TopView/>
+                <DividingLine/>
+                <BottomView/>
+            </LinearGradient>
         </View>
+        
     );
     
 }
